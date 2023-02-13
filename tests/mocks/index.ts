3c33da73e8fa903f74e9a202949a38917917b470
Will <williansilva3dsi@hotmail.com>
2023-02-13 17:02:18 -0300

    test: CarService

---
+++ tests/mocks/index.ts
@@ -0,0 +1,44 @@
+export const mockCarResponse = {
+  id: '6348513f34c397abcad040b2',
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const mockCarRequest = {
+  model: 'Marea',
+  year: 2002,
+  color: 'Black',
+  status: true,
+  buyValue: 15.99,
+  doorsQty: 4,
+  seatsQty: 5,
+};
+
+export const mockCarListResponse = [
+  {
+    id: '634852326b35b59438fbea2f',
+    model: 'Marea',
+    year: 2002,
+    color: 'Black',
+    status: true,
+    buyValue: 15.99,
+    doorsQty: 4,
+    seatsQty: 5,
+  },
+  {
+    id: '634852326b35b59438fbea31',
+    model: 'Tempra',
+    year: 1995,
+    color: 'Black',
+    buyValue: 39,
+    doorsQty: 2,
+    seatsQty: 5,
+  },
+];
+
+export const mockMongoId = '634852326b35b59438fbea31';
---
+++ tests/unit/Services/carService.test.ts
@@ -0,0 +1,64 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import {
+  mockCarListResponse,
+  mockCarRequest,
+  mockCarResponse,
+  mockMongoId,
+} from '../../mocks';
+
+describe('Car Service implementation', function () {
+  it('Should register a car in the DB', async function () {
+    sinon.stub(CarModel, 'register').resolves(mockCarResponse);
+
+    const service = new CarServive();
+    const response = await service.register(mockCarRequest);
+
+    expect(response).to.be.deep.equal(mockCarResponse);
+  });
+
+  it('Should list all cars in the DB', async function () {
+    sinon.stub(CarModel, 'getAll').resolves(mockCarListResponse);
+
+    const service = new CarServive();
+    const response = await service.getAll();
+
+    expect(response).to.be.deep.equal(mockCarListResponse);
+  });
+
+  it('Should list a car by id in the DB', async function () {
+    sinon.stub(CarModel, 'getById').resolves(mockCarResponse);
+
+    const service = new CarServive();
+    const response = await service.getById(mockMongoId);
+
+    expect(response).to.be.deep.equal(mockCarResponse);
+  });
+
+  it('Should return an error if the car id is not found in the DB', async function () {
+    sinon.stub(CarModel, 'getById').resolves([]);
+
+    const service = new CarServive();
+    const response = await service.getById(mockMongoId);
+
+    expect(response).to.be.deep.equal({ error: 'Car not found' });
+  });
+
+  it('Should return an error if the car id is not a valid mongo id', async function () {
+    sinon.stub(CarModel, 'getById').resolves(null);
+
+    const service = new CarServive();
+    const response = await service.getById('123InvalidId');
+
+    expect(response).to.be.deep.equal({ error: 'Invalid mongo id' });
+  });
+
+  it('Should update a car by id in the DB', async function () {
+    sinon.stub(CarModel, 'updateById').resolves(mockCarResponse);
+
+    const service = new CarServive();
+    const response = await service.updateById(mockMongoId);
+
+    expect(response).to.be.deep.equal(mockCarResponse);
+  });
+});
