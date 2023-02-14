334c7ef68208f0dbe8dfa9340a9f2c33fec218fd
Will <williansilva3dsi@hotmail.com>
2023-02-14 14:39:21 -0300

    fix: inheritence on Car domain properties

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,14 +1,14 @@
 import ICar from '../Interfaces/ICar';
 
 export default class Car {
-  public id: string | undefined;
-  public model: string;
-  public year: number;
-  public color: string;
-  public buyValue: number;
-  readonly doorsQty: number;
-  readonly seatsQty: number;
-  public status?: boolean;
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected buyValue: number;
+  private _doorsQty: number;
+  private _seatsQty: number;
+  protected status?: boolean;
 
   constructor(car: ICar) {
     this.id = car.id;
@@ -16,8 +16,40 @@ export default class Car {
     this.year = car.year;
     this.color = car.color;
     this.buyValue = car.buyValue;
-    this.doorsQty = car.doorsQty;
-    this.seatsQty = car.seatsQty;
-    this.status = car.status || false;
+    this._doorsQty = car.doorsQty;
+    this._seatsQty = car.seatsQty;
+    this.status = car.status;
   }
+
+  // protected get doorsQty() {
+  //   return this._doorsQty;
+  // }
+
+  // protected get seatsQty() {
+  //   return this._seatsQty;
+  // }
+
+  // protected get id() {
+  //   return this._id;
+  // }
+
+  // protected get model() {
+  //   return this._model;
+  // }
+
+  // protected get year() {
+  //   return this._year;
+  // }
+
+  // protected get color() {
+  //   return this._color;
+  // }
+
+  // public get buyValue() {
+  //   return this._buyValue;
+  // }
+
+  // public get status() {
+  //   return this._status;
+  // }
 }
