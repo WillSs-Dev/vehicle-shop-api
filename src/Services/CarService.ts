f2e031f4ad0513278e3d9322d7b69e5c1b44b6c2
Will <williansilva3dsi@hotmail.com>
2023-02-13 21:44:42 -0300

    fix: logic loop when using the generateDomain method

--- src/Services/CarService.ts
+++ src/Services/CarService.ts
@@ -4,30 +4,23 @@ import CarModel from '../Models/CarModel';
 
 export default class CarService {
   private model: CarModel;
-  
+
   constructor() {
     this.model = new CarModel();
   }
 
-  private generateDomain = (newCar: ICar) => {
-    const carDomain = new Car(newCar);
-    return {
-      id: carDomain.id,
-      model: carDomain.model,
-      year: carDomain.year,
-      color: carDomain.color,
-      buyValue: carDomain.buyValue,
-      doorsQty: carDomain.doorsQty,
-      seatsQty: carDomain.seatsQty,
-      status: carDomain.status,
-    };
-  };
+  // private generateDomain = (newCar: ICar) => {
+  //   const newA = new Car(newCar);
+  //   console.log(newA);
+  //   return newA;
+  // };
+
+  private generateDomain = (newCar: ICar) => new Car(newCar);
 
   public register = async (carRequest: ICar) => {
     const { status } = carRequest;
     const carData = { ...carRequest, status: status || false };
     const createdCar = await this.model.register(carData);
-    const newCar = new Car(createdCar);
-    return this.generateDomain(newCar);
+    return this.generateDomain(createdCar);
   };
 }
