ab2e71a7c3ede8c34b9b191b88d12b3ebe4e4c77
Will <williansilva3dsi@hotmail.com>
2023-02-15 15:07:14 -0300

    refactor: vehicles inherit properties from abstract class Vehicle

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,55 +1,13 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-export default class Car {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected buyValue: number;
+export default class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
-  protected status?: boolean;
 
   constructor(car: ICar) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.buyValue = car.buyValue;
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
-    this.status = car.status;
   }
-
-  // protected get doorsQty() {
-  //   return this._doorsQty;
-  // }
-
-  // protected get seatsQty() {
-  //   return this._seatsQty;
-  // }
-
-  // protected get id() {
-  //   return this._id;
-  // }
-
-  // protected get model() {
-  //   return this._model;
-  // }
-
-  // protected get year() {
-  //   return this._year;
-  // }
-
-  // protected get color() {
-  //   return this._color;
-  // }
-
-  // public get buyValue() {
-  //   return this._buyValue;
-  // }
-
-  // public get status() {
-  //   return this._status;
-  // }
 }
