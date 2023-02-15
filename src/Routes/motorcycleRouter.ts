d0acc81f3940629db0825a3db80ba258805e6f82
Will <williansilva3dsi@hotmail.com>
2023-02-15 17:38:18 -0300

    fix: adding middlewate on motorcycle router

--- src/Routes/motorcycleRouter.ts
+++ src/Routes/motorcycleRouter.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import MotorcycleController from '../Controllers/MotorcycleController';
+import validateId from '../Middlewares/ValidateId';
 
 const motorcycleRouter = Router();
 
-motorcycleRouter.post('/', (req, res) => new MotorcycleController(req, res).register());
+motorcycleRouter.post('/', (req, res) =>
+  new MotorcycleController(req, res).register());
 
-motorcycleRouter.get('/', (req, res) => new MotorcycleController(req, res).getAll());
+motorcycleRouter.get('/', (req, res) =>
+  new MotorcycleController(req, res).getAll());
 
-motorcycleRouter.get('/:id', (req, res) => new MotorcycleController(req, res).getById());
+motorcycleRouter.get('/:id', validateId, (req, res) =>
+  new MotorcycleController(req, res).getById());
 
 export default motorcycleRouter;
