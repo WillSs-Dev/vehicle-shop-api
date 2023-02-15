cf7f52c40460ef978d3d7203c59b243a2df587fb
Will <williansilva3dsi@hotmail.com>
2023-02-15 15:23:03 -0300

    feat: interface inheritance

--- src/Interfaces/ICar.ts
+++ src/Interfaces/ICar.ts
@@ -1,10 +1,6 @@
-export default interface ICar {
-  id?: string,
-  model: string,
-  year: number,
-  color: string,
-  buyValue: number,
-  doorsQty: number,
-  seatsQty: number,
-  status?: boolean,
+import IVehicle from './IVehicle';
+
+export default interface ICar extends IVehicle {
+  doorsQty: number;
+  seatsQty: number;
 }
